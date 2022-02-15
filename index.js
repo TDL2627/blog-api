74950768e821d93945ac129ef203477b1b9e9a6b
TDL2627 <the.dark.lord.2627@gmail.com>
2022-02-15 16:19:34 +0200

    added blog api

--- index.js
+++ index.js
@@ -63,19 +63,41 @@ app.get('/users/:id', (req, res)=>{
     });
   });
 
-  //update 1 
-  app.put('/users/:id',(req, res) =>{
-    const {name, email, contact, password} =req.body;
-    if(!name || !email || !contact ||  !password)
-    res.status(400).send({msg:"not all data inserted"});
+  // //update 1 
+  // app.put('/users/:id',(req, res) =>{
+  //   const {name, email, contact, password, } =req.body;
+  //   if(!name || !email || !contact ||  !password)
+  //   res.status(400).send({msg:"not all data inserted"});
 
-         var sql = `UPDATE users  SET user_name = '${name}', user_email ='${email}', user_contact = '${contact}' , user_password = '${password}' WHERE user_id=${req.params.id} `;
-         con.query(sql, function (err, result) {
-           if (err) throw err;
-           console.log("1 record updated");
-           res.send(result)
-         });
-       });
+  //        var sql = `UPDATE users  SET user_name = '${name}', user_email ='${email}', user_contact = '${contact}' , user_password = '${password}' WHERE user_id=${req.params.id} `;
+  //        con.query(sql, function (err, result) {
+  //          if (err) throw err;
+  //          console.log("1 record updated");
+  //          res.send(result)
+  //        });
+  //      });
+
+  // update2.0
+  app.put('/users/:id', (req, res)=>{
+    const {name,email,contact,password,avatar,about} = req.body;
+    let id = req.params.id;
+      var sql = `UPDATE users SET`
+      if(name) {sql += ` user_name = '${name}' ,`}
+      if(email) {sql += ` user_email = '${email}',`}
+      if(contact) {sql += ` user_contact = '${contact}',`}
+      if(password) {sql += ` user_password = '${password}',`}
+      if(avatar) {sql += ` user_avatar = '${avatar}',`}
+      if(about) {sql += ` user_about = '${about}',`}
+      if(sql.endsWith(',')) sql = sql.substring(0, sql.length-1)
+      sql += ` WHERE user_id=${id}`
+      con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("User Updated!!!")
+        res.send(result)
+      });
+  });
+
+
 
 
 // delete user
@@ -96,12 +118,61 @@ app.post('/users',(req, res) =>{
          var sql = `INSERT INTO users (user_name, user_email, user_contact, user_password) VALUES ('${name}', '${email}',  '${contact}','${password}')`;
          con.query(sql, function (err, result) {
            if (err) throw err;
-           console.log("1 record inserted");
+           console.log("1 user inserted");
          });
        });
  
+
+
+       // get all blogs
+app.get('/blogs', (req, res)=>{
+  var sql = `SELECT * FROM posts`;
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Blogs Shown");
+    res.send(result)
+  });
+});
  
- 
+// add blog
+app.post('/blogs',(req, res) =>{
+  const {title, body, date, author} =req.body;
+  if(!title || !body || !date || !author )
+  res.status(400).send({msg:"not all data inserted"});
+       var sql = `INSERT INTO posts (post_title, post_body, post_date , post_author) VALUES ('${title}', '${body}', '${date}' , '${author}')`;
+       con.query(sql, function (err, result) {
+         if (err) throw err;
+         console.log("1 record inserted");
+         res.send(result);
+       });
+     });
+
+// delete a blog
+app.delete('/blogs/:id',(req, res) =>{
+  var sql = `DELETE FROM posts WHERE post_id=${req.params.id}`;
+       con.query(sql, function (err, result) {
+         if (err) throw err;
+         console.log("Blog Deleted");
+         res.send(result)
+       });
+     });
+
+//  update blog
+app.put('/blogs/:id', (req, res)=>{
+  const {title,body} = req.body;
+  let id = req.params.id;
+    var sql = `UPDATE posts SET`
+    if(title) {sql += ` post_title = '${title}' ,`}
+    if(body) {sql += ` post_body = '${body}',`}
+    if(sql.endsWith(',')) sql = sql.substring(0, sql.length-1)
+    sql += ` WHERE post_id=${id}`
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("Blog Updated!!!")
+      res.send(result)
+    });
+});
+
  
 
 app.listen(app.get("port"), () => {
